037ded8023eb8a793262a137a624af35c04611b2
jflinn18 <joeflinn412@gmail.com>
2015-06-03 15:26:35 -0500

    Will now catch non-existent files
    I did discover a problem with our implementation of the algorithm.

--- graph_creationF.py
+++ graph_creationF.py
@@ -1,14 +1,23 @@
 from Graph import *
 
 
+def get_file():
+	try:
+		print "Enter graph file:",
+		fname = raw_input()
+
+		file = open(fname, 'r')
+
+		for line in file:
+			s = line.split()
+			g.addEdge(s[0], s[1], int(s[2]))
+			
+		file.close()
+	except:
+		print "---File does not exist---"
+		get_file()
+		
+
+
 g = Graph()
-print "Enter graph file:",
-fname = raw_input()
-
-file = open(fname, 'r')
-
-for line in file:
-	s = line.split()
-	g.addEdge(s[0], s[1], int(s[2]))
-	
-file.close()
+get_file()
