f1e436a27fb5e6fa2d0bd85b1e6f929e3a5624c3
jflinn18 <joeflinn412@gmail.com>
2015-06-26 15:12:23 -0700

    Removed my automation experiment
    I'll add an automation module if my experiments work

--- Automation.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-i = 0
-
-while i < 5:
-	os.system('python numExp.py')
-	os.system(10)
-	i = i + 1
--- numExp.py
+++
@@ -1,3 +0,0 @@
-print "Enter a number: ",
-num = raw_input()
-print str(int(num) + 10) + ' is your number plus 10'
