fb482f7672ad6e411c2b4a7854c56ccf440bee6c
Joseph <joeflinn412@gmail.com>
2015-07-15 13:05:46 -0500

    rewrote file_counter.py

--- file_counter.py
+++ file_counter.py
@@ -1,12 +1,26 @@
 import os
-
-directory = raw_input("Enter the directory: ")
-
-files = os.listdir(directory)
-
-i = 0
-for f in files:
-    i += 1
+import time
 
 
-print "Number of files in " + directory + " is: " + str(i)
+def num_files(directory):
+    i = 0
+    for f in directory:
+        i += 1
+
+    return i
+
+
+input_directory = raw_input("Enter the input directory: ")
+data_directory = raw_input("Enter the data directory: ")
+
+
+target_num = num_files(os.listdir(input_directory))
+
+print "Number of files in " + input_directory + " is: " + str(target_num)
+
+while True:
+    print "Number of files in " + data_directory + " is: " + str(num_files(data_directory))
+    
+    time.sleep(60)
+
+
