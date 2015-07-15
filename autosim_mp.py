aeef252e10e0a15b2e757cc205cea098e56e76cf
Joseph <joeflinn412@gmail.com>
2015-07-15 14:16:03 -0500

    Multiprocessing Worksls!

--- autosim_mp.py
+++ autosim_mp.py
@@ -2,13 +2,28 @@ import os
 import multiprocessing as mp
 import pdb
 from time import *
-import Queue
 import threading
+import subprocess as sp
 
 
 def run_auto(f):
     try:
-        os.system('python shortest_path.py -f ' +  directory + '/' + f + ' -i 1000 > data/' + alg[1] + '/' + alg[-1] + '/rawdata_0/' + f)
+        cmd = 'python shortest_path.py -f ' +  directory + '/' + f + ' -i 1000'
+        #os.system('python shortest_path.py -f ' +  directory + '/' + f + ' -i 1000 > data/' + alg[1] + '/' + alg[-1] + '/rawdata_0/' + f)
+        #cmd = 'python shortest_path.py'
+        #arg1 = '-f'
+        #arg2 = directory + '/' + f 
+        #arg3 = '-i'
+        #arg4 = '1000'
+
+        out = sp.check_output(cmd, shell=True)
+        s = []
+        s.append(f)
+        s.append(out)
+        return s
+        
+    except sp.CalledProcessError:
+        print '-----CalledProcessError-----'
 
     except:
         print "-----Error-----"
@@ -40,17 +55,33 @@ alg = directory.split('/')
 
 dirs = os.listdir(directory)
 
-pool = mp.Pool(processes = len(dirs))
-#[pool.map(run_auto, f) for f in dirs]
-results = [pool.apply_async(run_auto, args=(f,)) for f in dirs]
-#[pool.apply_async(run_auto, args=(f,)) for f in dirs]
-#output = (p.get() for p in results)
+#q = mp.Queue()
+results = []
+
+pool = mp.Pool(processes = 4)
+#output = [pool.map(run_auto, f) for f in dirs]
+#results = [pool.apply_async(run_auto, args=(f)) for f in dirs]
+
+for f in dirs:
+    #pdb.set_trace()
+    results.append(pool.apply_async(run_auto, args=(f,)))
 
 
+
+for i in results:
+    item = i.get()
+    outfile = open('data/' + alg[1] + '/' + alg[-1] + '/rawdata_0/' + item[0], 'wb')
+
+    outfile.write(item[1])
+    outfile.close()
+
+
+
+#for f in dirs:
+    #run_auto(f)
 
 end = time()
 
-for r in results:
-    print r
+#print 'Length of queue: ' + q.qsize()
 
 print 'Time: ' + str(end - start)
